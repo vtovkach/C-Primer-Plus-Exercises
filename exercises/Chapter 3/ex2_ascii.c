a0d8c5c109538e910dced4af5f10aa7b86639f1d
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-12-30 12:03:00 -0600

    Add files via upload

--- exercises/Chapter 3/ex2_ascii.c
+++ exercises/Chapter 3/ex2_ascii.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    char userInput;
+
+    printf("Enter the ascii code value: ");
+    scanf("%d", &userInput);
+
+    printf("The corresponding character is %c.\n", userInput);
+
+    return 0;
+}
