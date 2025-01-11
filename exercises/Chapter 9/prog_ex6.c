5836133c645c26f77b34bdf46b2e7f3fcc3d6067
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-01-11 12:31:16 -0600

    Programming Exercise 6, Chapter 9

---
+++ exercises/Chapter 9/prog_ex6.c
@@ -0,0 +1,43 @@
+/* Programming Exercise 9.6 */
+
+#include <stdio.h>
+
+void sort(double *, double *, double *); 
+
+int main(void)
+{
+    double x, y, z;
+
+    x = 3;
+    y = 3;
+    z = 7;
+
+    sort_two(&x, &y, &z);
+
+    printf("x:%.0lf | y: %.0lf | z: %.0lf\n", x, y, z);
+    return 0;
+}
+
+void sort_two(double *a, double *b, double *c)
+{
+    double temp;
+
+    if(*a > *b)
+    {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    if(*b > *c)
+    {
+        temp = *b;
+        *b = *c;
+        *c = *b;
+    }
+    if(*a > *b)
+    {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
