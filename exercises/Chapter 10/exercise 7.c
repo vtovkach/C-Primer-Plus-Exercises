57a63768b8c14e720e2c6f1ee9fac3e265ee55c3
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-01-23 22:30:58 -0600

    Add files via upload

--- exercises/Chapter 10/exercise 7.c
+++ exercises/Chapter 10/exercise 7.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-#define ROWS     2
+#define ROWS     3
 #define COLUMNS  3
 
 // copy function from the Exercise 2 
@@ -10,16 +10,30 @@ void copy_array_two(double *target, const double *source, size_t size);
 
 int main(void)
 {
-    double source_2D[ROWS][COLUMNS] = {{1.2, 3.7, 9.9}, {0.9, 5.5, 8.7}};
+    double source_2D[ROWS][COLUMNS] = {{1.2, 3.7, 9.9}, {0.9, 5.5, 8.7}, {1.1, 6.6, 4.1}};
 
     double target_2D[ROWS][COLUMNS];
 
     // copying first subarray
-    copy_array_two(target_2D, source_2D, COLUMNS);
+    copy_array_two(*target_2D, source_2D[0], COLUMNS);
     
     // copying second subarray
-    copy_array_two(target_2D, source_2D, COLUMNS);
-    
+    copy_array_two(*(target_2D + 1), source_2D[1], COLUMNS);
+
+    // copying third subarray
+    copy_array_two(*(target_2D + 2), source_2D[2], COLUMNS);
+
+    // *(target_2D + 2) the same as target_2D[2]; 
+
+    for(int i = 0; i < ROWS; ++i)
+    {
+        for(int j = 0; j < COLUMNS; ++j)
+        {
+            printf("%.1lf ", target_2D[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
 
