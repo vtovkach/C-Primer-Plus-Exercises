3f16525f05bae82e7ef865cda0ce3c986ddd42e6
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-02-11 20:28:58 -0600

    Programming Exercise 3, Chapter 11

---
+++ exercises/Chapter 11/exercise 3.c
@@ -0,0 +1,53 @@
+/* Programming Exercise 11.3 */
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define LIM 10
+
+char *get_s(char *);
+
+int main(void)
+{
+    char string[LIM];
+    char *status;
+
+    puts("Enter text: ");
+
+    status = get_s(string);
+    
+    if(status)
+        printf("Here is the string: %s\n", string);
+    else
+        puts("No input was entered!");
+
+    return 0;
+}
+
+char *get_s(char *dest)
+{
+    int ch;
+    int i = 0;
+
+    while((ch = getchar()), !(isspace(ch)))
+    {
+        *(dest + i) = ch;
+        i++;
+    }
+
+    // Discard the rest of the line
+    if(ch != '\n' && ch != EOF)
+    {
+        while(getchar() != '\n')
+            continue;
+    }
+
+    // Add null terminator to the string
+    *(dest + i) = '\0';
+
+    // Return NULL if no input was processed 
+    if(i == 0)
+        return NULL;
+
+    return dest;
+}
