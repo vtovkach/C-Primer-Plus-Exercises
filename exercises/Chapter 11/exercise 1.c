658076b5986dab063e702fcbe6a547bf415dbc4d
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-02-11 18:27:55 -0600

    Programming Exercise 1, Chapter 11

--- exercises/Chapter 11/exercise 1.c
+++ exercises/Chapter 11/exercise 1.c
@@ -1,1 +1,48 @@
+/* Programming Exercises 11-1 */
 
+// Design and test a function that fetches the next n characters from input (including
+// blanks, tabs, and newlines), storing the results in an array whose address is passed as an
+// argument.
+
+
+#include <stdio.h>
+
+#define LIM 10
+
+char * get_n_input(char *, int);
+
+int main(void)
+{
+    char input[LIM];
+    char *check;
+
+    printf("Enter some text: ");
+    check = get_n_input(input, LIM);
+
+    if (check == NULL)
+        puts("Input failed.");
+    else
+        printf("Here is output: %s\n", input);
+
+    puts("Done.");
+
+    return 0;
+}
+
+char *get_n_input(char *dest, int n)
+{   
+    int ch;
+    int i = 0;  
+
+    while((ch = getchar()) != '\0' && ch != EOF && ch != '\n' && i < n - 1)
+    {
+        dest[i++] = ch;
+    }
+
+    dest[i] = '\0';
+    
+    if(i == 0)
+        return NULL;
+
+    return dest;
+}
