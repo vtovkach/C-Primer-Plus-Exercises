94cf386847b010fa340e2504cf388feeea329eb0
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-02-13 19:25:09 -0600

    pe12-2a.h & pe12-2a.c

---
+++ exercises/Chapter 12/Exercise 2/pe12-2a.c
@@ -0,0 +1,58 @@
+// pe12-2a.c 
+
+#include <stdio.h>
+
+static int mode = 0;
+static float distance;
+static float fuel_consumed;
+
+void set_mode(int mode_i)
+{
+    if(mode_i != 0 && mode_i != 1)
+    {
+        printf("You entered invalid mode. Program will use the most recent one.\n");
+        printf("Program uses European Mode by default.\n");
+    }
+    else
+    {
+        mode = mode_i;
+    }
+}
+
+void get_info(void)
+{
+
+    // MODE 0: Metric Mode (liters per 100 km) 
+    // MODE 1: US Mode     (miles  per gallon)
+
+    if(mode == 0)
+    {
+        printf("Please enter distance you traveled in km: ");
+        scanf("%f", &distance);
+        printf("Please enter the number of liters consumed: ");
+        scanf("%f", &fuel_consumed);
+    }
+    else
+    {
+        printf("Please enter distance you traveled in miles: ");
+        scanf("%f", &distance);
+        printf("Please enter the number of liters consumed: ");
+        scanf("%f", &fuel_consumed);
+    }
+}
+
+void show_info(void)
+{
+    float consumption = 0;
+
+    if(mode == 0)
+    {
+        consumption = (100 / distance) * fuel_consumed;
+        printf("Your fuel consumption is %.2f liters per 100 km.\n", consumption);
+    }
+    else
+    {
+        consumption = distance / fuel_consumed;
+        printf("Your fuel consumption is %.2f miled per gallon.\n", consumption);
+    }
+}
---
+++ exercises/Chapter 12/Exercise 2/pe12-2a.h
@@ -0,0 +1,10 @@
+#ifndef _PE12_2A_H
+#define _PE12_2A_H
+
+extern void set_mode(int);
+
+extern void get_info(void);
+
+extern void show_info(void);
+
+#endif
