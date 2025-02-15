24d7341977961e96b2fe334417083de6e446e360
Vadym <107150489+vtovkach@users.noreply.github.com>
2025-02-15 01:18:40 -0600

    Completed Programming Exercise 5, Chapter 12

--- exercises/Chapter 12/exercise 5.c
+++ exercises/Chapter 12/exercise 5.c
@@ -8,22 +8,55 @@
 
 int random_num(int, int);
 
-int main()
+void sort_nums(int *, int);
+
+int main(void)
 {
-    int random_num[ARRAY_SIZE];
+    srand(time(NULL));
+
+    int random_nums[ARRAY_SIZE];
 
     for(int i = 0; i < ARRAY_SIZE; ++i)
     {
-        random_num[i] = random_num(1, 10);
+        random_nums[i] = random_num(1, 10);
+    }
+
+    sort_nums(random_nums, ARRAY_SIZE);
+
+    for(int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        if((i % 20) == 0)
+            putchar('\n');
+
+        printf("%d ", random_nums[i]);
     }
 
     return 0;
 }
 
-
 int random_num(int min, int max)
 {
-    srand(time(NULL));
+    return (rand() % (max - min + 1)) + min;
+}
 
-    return 10;
+void sort_nums(int *nums, int size)
+{
+    // Bubble Sort Algorithm 
+
+    int temp;
+    int i;
+    int j;
+
+    for(i = 0; i < size - 1; i++)
+    {
+        for(j = 0; j < size - i - 1; j++)
+        {
+            if(*(nums + j) > *(nums + j + 1))
+            {
+                temp = *(nums + j);
+                *(nums + j) = *(nums + j + 1);
+                *(nums + j + 1) = temp;
+            }
+        }
+    }
 }
