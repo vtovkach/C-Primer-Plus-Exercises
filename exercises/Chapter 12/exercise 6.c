75e6e9f7d37e0a524d5ec7a9a841c950ac8017e6
Vadym <107150489+vtovkach@users.noreply.github.com>
2025-02-15 16:01:59 -0600

    Add files via upload

---
+++ exercises/Chapter 12/exercise 6.c
@@ -0,0 +1,40 @@
+/* Programming Exercise 12.6 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TOTAL_NUMS 10
+#define MIN        1
+#define MAX        10
+
+int get_random_num(int, int);
+
+int main(void)
+{
+    srand(time(NULL));
+
+    int occurances[MAX] = {};        
+    int rand_num;
+
+    for(int i = 0; i < TOTAL_NUMS; ++i)
+    {
+        rand_num = get_random_num(MIN, MAX);
+        printf("%d ", rand_num);
+        occurances[rand_num - 1] = occurances[rand_num - 1] + 1; 
+    }
+
+    putchar('\n');
+
+    for(int i = 0; i < MAX; ++i)
+    {
+        printf("Number %d was generated %d times.\n", i + 1, occurances[i]);
+    }
+
+    return 0;
+}
+
+int get_random_num(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
