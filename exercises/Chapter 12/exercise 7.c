4b768e201c48ef687dd43d767952d6add14d89c7
Vadym <107150489+vtovkach@users.noreply.github.com>
2025-02-15 19:48:45 -0600

    Programming Exercise 7, Chapter 12 (Completed)

--- exercises/Chapter 12/exercise 7.c
+++ exercises/Chapter 12/exercise 7.c
@@ -13,10 +13,11 @@ int main(void)
     int dice;
     int status;
 
+    srand(time(0));
+
     printf("Enter the number of sets; Enter q to stop: ");
     while(scanf("%d", &sets) == 1)
     {
-
         printf("How many sides and how many dice? ");
         if((status = scanf("%d %d", &sides, &dice)) != 2)
         {
@@ -35,13 +36,30 @@ int main(void)
             }
         }
 
-        printf("Hear is %d ")
+        printf("Hear are %d sets of %d %d-sided throws.", sets, dice, sides);
+        for(int i = 0; i < sets; ++i)
+        {
+            if((i % 15) == 0)
+                putchar('\n');
 
+            printf("%3d ", roll_dice(sides, dice));
+        }
+        putchar('\n');
 
-
-
-
+        printf("Enter the number of sets; Enter q to stop: ");
     }
-
     return 0;
+}
+
+int roll_dice(int side, int dice)
+{
+    int total = 0;
+    int random_num;
+
+    for(int i = 0; i < dice; ++i)
+    {
+        random_num = (rand() % side) + 1;
+        total += random_num;
+    }
+    return total;
 }
